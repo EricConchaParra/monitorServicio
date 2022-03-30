bfc6db3ca214619a6fb65d7d352a1ce73167d595
Eric Concha Parra <hola@ericconcha.com>
2022-03-30 01:58:17 -0300

    Avances con props to component Estoy leyendo los props enviados desde serviceTable a miCard, tengo problemas con las fechas Necesito calcular cuanto tiempo falta para entregar, y si faltan menos de 30 minutos avisar Si está atrazado avisar.

--- src/components/miCard.vue
+++ src/components/miCard.vue
@@ -1,13 +1,16 @@
 <template>
   <div class="miCard__contenedor">
-    <div class="id">Camioneta Chevrolet <b>DTTK-37</b></div>
+    <div class="encabezado error">
+      {{ this.vehiculo.tipo }} {{ this.vehiculo.marca }}
+      <b>{{ this.vehiculo.patente }}</b>
+    </div>
     <div class="detalles">
       <div class="operario">
-        <div class="id_operario">Operario: Joaquín Concha Jara</div>
-        <div class="entrega">Entrega: 12:00 hrs</div>
+        <div class="id_operario">Operario: {{ this.vehiculo.operario }}</div>
+        <div class="entrega">Entrega: {{ this.vehiculo.entrega }}</div>
       </div>
-      <div class="estado" style="background-color: blue">
-        <p>En servicio</p>
+      <div class="estado">
+        <div class="textoEstado">{{ textoEstado }}</div>
       </div>
     </div>
   </div>
@@ -16,5 +19,49 @@
 <script>
 export default {
   name: "miCard",
+
+  props: {
+    vehiculo: Object,
+  },
+
+  data() {
+    return {
+      //estado: servicio, finalizar, atrazado
+      textoEstado: "",
+    };
+  },
+
+  mounted() {
+    console.log("Montado ;)");
+  },
+
+  watch: {},
+
+  computed: {
+    textoEstado() {
+      let entrega = new Date(this.vehiculo.entrega);
+      let horaActual = new Date();
+      let tiempoRestante;
+      let text;
+
+      console.log(entrega);
+      console.log(horaActual);
+
+      tiempoRestante = (entrega - horaActual) / 1000;
+
+      tiempoRestante = new Date(tiempoRestante);
+
+      console.log(tiempoRestante);
+
+      if (tiempoRestante.getMinutes() < 0) {
+        text = "Atrazado";
+      } else if (tiempoRestante.getMinutes() <= 30) {
+        text = "Finalizando";
+      } else {
+        text = "En Servicio";
+      }
+      return text;
+    },
+  },
 };
 </script>
--- src/components/serviceTable.vue
+++ src/components/serviceTable.vue
@@ -1,99 +1,11 @@
 <template>
   <content>
-    <miCard />
-    <miCard />
-    <miCard />
-    <miCard />
-    <miCard />
-    <miCard />
-    <miCard />
-    <miCard />
-    <miCard />
-    <miCard />
-    <miCard />
-    <miCard />
-
-    <!-- <table>
-      <tr>
-        <th>Pantente</th>
-        <th>Tipo Vehículo</th>
-        <th>Operario</th>
-        <th>Hora entrega</th>
-        <th>Estado</th>
-      </tr>
-
-      <tr class="error">
-        <td>LOGD-52</td>
-        <td>Furgón Suzuki</td>
-        <td>Daniel Concha</td>
-        <td>12:20</td>
-        <td>Atención</td>
-      </tr>
-
-      <tr class="warning">
-        <td>DTTK-37</td>
-        <td>Automovil JAC</td>
-        <td>Eric Concha</td>
-        <td>13:00</td>
-        <td>En servicio</td>
-      </tr>
-
-      <tr>
-        <td>HJSD-62</td>
-        <td>Automovil Hyndai</td>
-        <td>Joaquín Concha</td>
-        <td>13:15</td>
-        <td>En servicio</td>
-      </tr>
-
-      <tr>
-        <td>DTSD-30</td>
-        <td>Camioneta Great Wall</td>
-        <td>Pedro Castro</td>
-        <td>14:15</td>
-        <td>En servicio</td>
-      </tr>
-
-      <tr>
-        <td>ASDS-23</td>
-        <td>Camioneta Toyota</td>
-        <td>Juan Pérez</td>
-        <td>14:20</td>
-        <td>En servicio</td>
-      </tr>
-
-      <tr class="completed">
-        <td>DTSD-27</td>
-        <td>Automovil Mitsubishi</td>
-        <td>Pablo Perez</td>
-        <td>12:55</td>
-        <td>Finalizado</td>
-      </tr>
-
-      <tr class="completed">
-        <td>DTDS-22</td>
-        <td>Automovil Hyndai</td>
-        <td>Pedro Perez</td>
-        <td>12:55</td>
-        <td>Finalizado</td>
-      </tr>
-
-      <tr class="completed">
-        <td>AADF-12</td>
-        <td>Camioneta Mitsubishi</td>
-        <td>Julián Perez</td>
-        <td>12:55</td>
-        <td>Finalizado</td>
-      </tr>
-
-      <tr class="completed">
-        <td>DTSD-27</td>
-        <td>Automovil Mitsubishi</td>
-        <td>Andrés Perez</td>
-        <td>12:55</td>
-        <td>Finalizado</td>
-      </tr>
-    </table> -->
+    <miCard
+      v-for="servicio in servicios"
+      :vehiculo="servicio"
+      :key="servicio.patente"
+    >
+    </miCard>
   </content>
 </template>
 
@@ -105,25 +17,38 @@ export default {
   components: { miCard },
 
   data() {
-    return {};
+    return {
+      servicios: [
+        {
+          patente: "DTTX-37",
+          marca: "Chevrolet",
+          tipo: "Camioneta",
+          operario: "Joaquin Concha",
+          entrega: "2022/03/30 01:40:00",
+        },
+        {
+          patente: "LGVJ-53",
+          marca: "Great Wall",
+          tipo: "Camioneta",
+          operario: "Daniel Concha",
+          entrega: "2022/03/30 01:05:00",
+        },
+        {
+          patente: "AA-2323",
+          marca: "Datsun",
+          tipo: "Automovil",
+          operario: "Juan Perez",
+          entrega: "2022/03/30 16:20:00",
+        },
+        {
+          patente: "DTTK-37",
+          marca: "Jac",
+          tipo: "Automovil",
+          operario: "Pedro Gonzalez",
+          entrega: "2022/03/30 17:20:00",
+        },
+      ],
+    };
   },
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
